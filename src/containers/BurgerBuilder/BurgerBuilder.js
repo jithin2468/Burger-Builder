d47f9f580cfb5b750488beb87a98a07cf21d31c3
MAF <maf@DES000447s-iMac.local>
2018-08-20 13:51:29 +0530

    Till hardcoded orders display and route

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,32 +94,11 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('YOU CONTINUE');
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Upendra Ramaswamy',
-        //         address: {
-        //             street: 'TestStreet 1',
-        //             zipCode: '560094',
-        //             country: 'India'
-        //         },
-        //         email: 'upendrabr.93@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //     });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
