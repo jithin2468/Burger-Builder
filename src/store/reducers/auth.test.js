5df1038992a4ced03ffd283e19cfb9dac26fa1e0
Upendra Ramaswamy <maf@DES000447s-iMac.local>
2019-03-11 18:24:24 +0530

    Deployment successful

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -11,6 +11,25 @@ describe('auth reducer', () => {
             authRedirectPath: '/'        
         })
     })
+    it('should store the token upon login', () => {
+        expect(reducer({            
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'        
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-userId'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-userId',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'  
+        })
+    })
 })
 
 
